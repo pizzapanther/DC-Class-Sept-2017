8187e2a723a50e8944ac19cb21b5203766d9d534
Paul Bailey <paul.m.bailey@gmail.com>
2017-09-13 09:22:48 -0500

    added matrix exercise

---
+++ py-exercises/matrix-multiply.py
@@ -0,0 +1,20 @@
+# [2, 4, 5] x [2, 3, 6] = [4, 12, 30]
+
+m1 = [2, 4, 5]
+m2 = [2, 3, 6]
+
+answer = []
+
+for i in range(0, len(m1)):
+  answer.append(m1[i] * m2[i])
+  
+print(answer)
+
+answer = []
+
+for i, num1 in enumerate(m1):
+  ans = num1 * m2[i]
+  answer.append(ans)
+  
+print(answer)
+
