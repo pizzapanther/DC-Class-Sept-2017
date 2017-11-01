8368b317a7c190a94f70c192171a14c76610cc3f
Paul Bailey <paul.m.bailey@gmail.com>
2017-11-01 10:52:21 -0500

    forms

--- js-exercises/express_example/app.js
+++ js-exercises/express_example/app.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
+var body_parser = require('body-parser');
+
+var pgp = require('pg-promise')({});
+
+var db = pgp({database: 'restaurant'});
 
 app.set('view engine', 'hbs');
 app.use('/static', express.static('public'));
+app.use(body_parser.urlencoded({extended: false}));
 
 app.get("/", function (request, response) {
   response.send('<html>');
@@ -36,6 +42,30 @@ app.get("/post/:slug", function (request, response) {
 
 app.get("/projects", function (request, response) {
   response.send('Projects');
+});
+
+app.get("/form", function (request, response) {
+  response.render('form.hbs', {title: 'Form'});
+});
+
+app.post("/submit", function (request, response) {
+  console.log(request.body);
+  //response.send('OK ' + request.body.name);
+  
+  response.redirect('/hello');
+});
+
+app.get('/search', function(req, resp, next) {
+  let term = req.query.searchTerm;
+  let query = "SELECT * FROM restaurant WHERE \
+  restaurant.name ILIKE '%$1#%'";
+  db.many(query, term)
+    .then(function(resultsArray) {
+      resp.render('search_results.hbs', {
+        results: resultsArray
+      });
+    })
+    .catch(next);
 });
 
 app.listen(8000, function () {
